e613e250b858096ca6841cc28e79fd32edaec056
Adrian Alvarez Galicia <adrianalvarezgalicia@MacBook-Pro.local>
2020-07-19 19:54:00 -0500

    analogic read correct works

--- src/nairda.cpp
+++ src/nairda.cpp
@@ -69,10 +69,6 @@ public:
     #else
       char tempread = analogRead(pin)/10;
     #endif
-    #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega2560__) || defined(__AVR_ATmega1280__)
-      Serial1.write((tempread >= 100) ? 100 : tempread);
-    #endif
-
     Serial.write((tempread >= 100) ? 100 :(tempread<0)?0: tempread);
     /*delay(5);
      #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega2560__) || defined(__AVR_ATmega1280__)
