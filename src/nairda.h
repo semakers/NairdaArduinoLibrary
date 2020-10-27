12ef1d7c56a69f8a2d78e627b64a062f16a32454
Adrian Alvarez <adrianalvarez@Adrians-iMac.local>
2020-10-27 09:35:26 -0700

    component class declarated

--- src/nairda.h
+++ src/nairda.h
@@ -45,6 +45,145 @@
 
 #define CURRENT_VERSION 1
 
+enum
+{
+  SERVO = 0,
+  MOTOR,
+  LED,
+  DIGITAL,
+  ANALOGIC,
+  ULTRASONIC
+};
+
+class component{
+public:
+  //analogc digital led
+  uint8_t pin;
+  //ultrasonic
+  NewPing *sonar;
+  //servo
+  Servo servo;
+  //dcMotor
+  uint8_t a, b, pwm, vel;
+
+  component(uint16_t *args)
+  {
+    switch (args[0])
+    {
+    case SERVO:
+      servo.attach(args[1], args[2], args[3]);
+      servo.write(args[4]);
+      break;
+    case MOTOR:
+      pinMode(args[1], OUTPUT);
+      pinMode(args[2], OUTPUT);
+      SoftPWMSet(args[3], 0);
+      a = args[1];
+      b = args[2];
+      pwm = args[2];
+      break;
+    case LED:
+      pinMode(args[1], OUTPUT);
+      SoftPWMSet(args[1], 0);
+      pin = args[1];
+      break;
+    case DIGITAL:
+      pin = args[1];
+      pinMode(pin, INPUT);
+      break;
+    case ANALOGIC:
+      pin = args[1];
+      break;
+    case ULTRASONIC:
+      sonar = new NewPing(args[1], args[2], 100);
+      break;
+    }
+  }
+
+  void execAct(uint8_t *execArgs, uint8_t type)
+  {
+    switch (type)
+    {
+    case SERVO:
+      servo.write((execArgs[0] < 0) ? 0 : (execArgs[0] > 180) ? 180 : execArgs[0]);
+      break;
+    case MOTOR:
+      vel = (execArgs[0] < 0) ? 0 : (execArgs[0] > 100) ? 100 : execArgs[0];
+      switch (execArgs[1])
+      {
+      case 0:
+        digitalWrite(a, HIGH);
+        digitalWrite(b, LOW);
+        SoftPWMSetPercent(pwm, vel);
+
+        break;
+      case 1:
+        digitalWrite(a, LOW);
+        digitalWrite(b, LOW);
+        SoftPWMSetPercent(pwm, 0);
+        break;
+      case 2:
+        digitalWrite(a, LOW);
+        digitalWrite(b, HIGH);
+        SoftPWMSetPercent(pwm, vel);
+        break;
+      }
+      break;
+    case LED:
+      SoftPWMSetPercent(pin, (execArgs[0] < 0) ? 0 : (execArgs[0] > 100) ? 100 : execArgs[0]);
+      break;
+    }
+  }
+
+  uint8_t getSensVal(uint8_t type)
+  {
+    uint8_t tempRead;
+    switch (type)
+    {
+    case DIGITAL:
+      tempRead = map(analogRead(pin), 0, 1023, 0, 100);
+      break;
+    case ANALOGIC:
+      tempRead = digitalRead(pin);
+      break;
+    case ULTRASONIC:
+      tempRead = sonar->ping_cm();
+      break;
+    }
+    return (tempRead < 0) ? 0 : (tempRead > 100) ? 100 : tempRead;
+  }
+
+  void sendSenseVal(uint8_t type)
+  {
+#if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega2560__) || defined(__AVR_ATmega1280__)
+    Serial1.write((char)getSensVal(type));
+#endif
+    Serial.write((char)getSensVal(type));
+  }
+
+  void off(uint8_t type)
+  {
+    switch (type)
+    {
+    case SERVO:
+      servo.detach();
+      break;
+    case ULTRASONIC:
+      free(sonar);
+      break;
+    case MOTOR:
+      SoftPWMEnd(a);
+      SoftPWMEnd(b);
+      SoftPWMEnd(pwm);
+      break;
+    case LED:
+      SoftPWMSet(pin, 0);
+      SoftPWMEnd(pin);
+      break;
+    }
+  }
+
+};
 
 class analogic
 {
